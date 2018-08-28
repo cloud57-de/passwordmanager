18cedb4489d61da887d34d140ebbf3def0fbbeef
Dirk Guenther <d.guenther@cloud57.de>
2018-08-28 08:35:33 +0200

    Model added

--- src/index.js
+++ src/index.js
@@ -2,27 +2,49 @@ import DriveAppsUtil from 'drive-apps-util';
 import URLSafeBase64 from 'urlsafe-base64';
 import MarterialDesign from 'material-design-lite';
 import doT from 'dot';
+import {PasswordList, PasswordModel} from './model';
+import Clipboard from 'clipboard';
+
+let cardTemplate = doT.template(document.getElementById("card-template").innerHTML);
+let pwdList = new PasswordList();
+new Clipboard(".clipboard");
 
 
 document.getElementById('splash').style.visibility = "hidden";
 
 document.querySelector("#bt_new").addEventListener('click', (e) => {
-  var cardTemplate = doT.template(document.getElementById("card-template").innerHTML);
-  var data = {
-    name: document.querySelector("#newname").value,
-    account: document.querySelector("#newaccount").value,
-    password: document.querySelector("#newpassword").value
-  }
-  var newElement = document.createElement("div");
-  newElement.innerHTML = cardTemplate(data);
-  document.querySelector('.mdl-grid').appendChild(newElement.firstElementChild);
-
-  document.querySelector("#newname").value = "";
-  document.querySelector("#newaccount").value = "";
-  password: document.querySelector("#newpassword").value = "";
-  componentHandler.upgradeAllRegistered();
+  var name = document.querySelector("#newname").value;
+  var account = document.querySelector("#newaccount").value;
+  var password = document.querySelector("#newpassword").value;
+   
+  var data = new PasswordModel(name, account, password);
+ 
+  pwdList.add(data);
+  document.querySelector("#newname").value="";
+  document.querySelector("#newaccount").value="";
+  document.querySelector("#newpassword").value="";
+  document.querySelector("#newname").parentElement.classList.remove("is-dirty");
+  document.querySelector("#newaccount").parentElement.classList.remove("is-dirty");
+  document.querySelector("#newpassword").parentElement.classList.remove("is-dirty");
 });
 
+let addNewItem = function(index, newItem) {
+  var newElement = document.createElement("div");
+  newItem.index=index;
+  newElement.innerHTML = cardTemplate(newItem);
+  newElement.querySelector(".bt_delete").addEventListener('click', (e) => {
+    var index = e.currentTarget.getAttribute("index");
+    pwdList.remove(index);
+  });
+  document.querySelector('.mdl-grid').appendChild(newElement.firstElementChild);
+}
+pwdList.registerAddListener(addNewItem);
+
+let addRemoveItem = function(index, removeItem) {
+  var element = document.querySelector("#card_" + index);
+  element.parentElement.removeChild(element);
+}
+pwdList.registerRemoveListener(addRemoveItem);
 
 
 let options = {
@@ -38,71 +60,19 @@ let driveAppsUtil = new DriveAppsUtil(options);
 driveAppsUtil.init().then(() => {
   driveAppsUtil.login().then((user) => {
     showUserImage();
+    loadPasswordDB();
+    showInfoMessage("Password DB loaded");
+
   });
 });
 
-window.save = () => {
-  convertToPNG().then((img) => {
-    editor.saveXML({ format: true }, function (err, xml) {
-      let metadata = JSON.stringify({
-        name: document.getElementById('docinfo').value,
-        mimeType: "application/bpmn+xml",
-        contentHints: {
-          thumbnail: {
-            image: img,
-            mimeType: "image/png"
-          }
-        }
-      });
-  
-      driveAppsUtil.updateDocument(id, metadata, xml).then((fileinfo) => {
-        document.getElementById('docinfo').value = fileinfo.name;
-        document.getElementById('docinfodrawer').textContent = fileinfo.name;
-        document.title = fileinfo.name;
-        window.localStorage.setItem("bpmndoctitle", fileinfo.name);
-        showInfoMessage("BPMN model stored")
-      });
-    });
-  });
+document.querySelector("#bt_save").addEventListener('click', (e) => {
+  localStorage.setItem("passworddb", pwdList.export());  
+  showInfoMessage("Password saved");
+});
 
-}
-
-function create(folderId) {
-  let initialDiagram =
-  '<?xml version="1.0" encoding="UTF-8"?>' +
-  '<bpmn:definitions xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' +
-                    'xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" ' +
-                    'xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" ' +
-                    'xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" ' +
-                    'targetNamespace="http://bpmn.io/schema/bpmn" ' +
-                    'id="Definitions_1">' +
-    '<bpmn:process id="Process_1" isExecutable="false">' +
-      '<bpmn:startEvent id="StartEvent_1"/>' +
-    '</bpmn:process>' +
-    '<bpmndi:BPMNDiagram id="BPMNDiagram_1">' +
-      '<bpmndi:BPMNPlane id="BPMNPlane_1" bpmnElement="Process_1">' +
-        '<bpmndi:BPMNShape id="_BPMNShape_StartEvent_2" bpmnElement="StartEvent_1">' +
-          '<dc:Bounds height="36.0" width="36.0" x="173.0" y="102.0"/>' +
-        '</bpmndi:BPMNShape>' +
-      '</bpmndi:BPMNPlane>' +
-    '</bpmndi:BPMNDiagram>' +
-  '</bpmn:definitions>';
-  let metadata = JSON.stringify({
-    name: "New Process",
-    mimeType: "application/bpmn+xml",
-    parents: [folderId]
-  });
-  driveAppsUtil.createDocument(metadata, initialDiagram).then((fileinfo) => {
-    id = fileinfo.id;
-    window.localStorage.setItem("bpmndoc", initialDiagram);
-    loadViewer(initialDiagram);
-    document.getElementById('docinfo').value = fileinfo.name;
-    document.getElementById('docinfodrawer').textContent = fileinfo.name;
-    document.title = fileinfo.name;
-    window.localStorage.setItem("bpmndoctitle", fileinfo.name);
-    showInfoMessage("New BPMN process created")
-
-  });
+function loadPasswordDB() {
+  pwdList.import(localStorage.getItem("passworddb"));
 }
 
 function showUserImage() {
@@ -111,18 +81,6 @@ function showUserImage() {
   document.getElementById('userimage').src = window.gapi.auth2.getAuthInstance().currentUser.get().getBasicProfile().getImageUrl();
 }
 
-function loadViewer(text) {
-}
-
-var done = function (err) {
-
-  if (err) {
-    showErrorMessage('Error loading password db: ' + err);
-  }
-  else {
-    showInfoMessage('Password db loaded');
-  }
-};
 
 function showErrorMessage(message) {
   if (typeof message.status !== 'undefined') {
---
+++ src/model.js
@@ -0,0 +1,69 @@
+export class PasswordList {
+
+    constructor() {
+        this.items = new Array();
+        this.addListener = new Array();
+        this.removeListener = new Array();
+    }
+
+    add(item) {
+        var index = this.items.push(item);
+        this.addListener.forEach((listener) => {
+            listener(index, item);
+        });
+    }
+
+    remove(index) {
+        this.removeListener.forEach((listener) => {
+            listener(index, this.items[index]);
+        }, this);
+        this.items.splice(index, 1);
+    }
+
+    list() {
+        return this.items;
+    }
+
+    export() {
+        return JSON.stringify(this.items);
+    }
+
+    import(data) {
+        if (data) {
+            var newData = JSON.parse(data);
+            newData.forEach((item) => {
+                this.add(item);
+            }, this);
+        }
+    }
+
+    registerAddListener(callback) {
+        this.addListener.push(callback);
+    }
+
+    unregisterAddListener(callback) {
+        var index = this.addListener.indexOf(callback);
+        this.addListener.splice(index, 1);
+    }
+
+    registerRemoveListener(callback) {
+        this.removeListener.push(callback);
+    }
+
+    unregisterRemoveListener(callback) {
+        var index = this.removeListener.indexOf(callback);
+        this.removeListener.splice(index, 1);
+    }
+
+}
+
+export class PasswordModel {
+
+    constructor(index, name, account, password) {
+        this.index = index;
+        this.name = name;
+        this.account = account;
+        this.password = password;
+    }
+
+}
